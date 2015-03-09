a1f4b520e726f14c51cfcd66b475643ba8ad8579
Chang Liu <shampliu@gmail.com>
2015-03-09 05:26:10 -0700

    checkpoint

--- Projects/4. steg-o-matic/4. steg-o-matic/BinaryConverter.cpp
+++ Projects/4. steg-o-matic/4. steg-o-matic/BinaryConverter.cpp
@@ -1,92 +1,93 @@
-//#include "provided.h"
-//#include <string>
-//#include <vector>
-//using namespace std;
-//
-//const size_t BITS_PER_UNSIGNED_SHORT = 16;
-//
-//string convertNumberToBitString(unsigned short number);
-//bool convertBitStringToNumber(string bitString, unsigned short& number);
-//
-//string BinaryConverter::encode(const vector<unsigned short>& numbers)
-//{
-//    string result = "";
-//    for (auto& num : numbers) {
-//        result += convertNumberToBitString(num);
-//    }
-//    
-//    for (int i = 0; i < result.length(); i++) {
-//        if (i == '1') {
-//            result[i] = '\t';
-//        }
-//        else if (i == '0') {
-//            result[i] = ' ';
-//        }
-//        // shouldn't be any other character
-//        else {
-//            return "";
-//        }
-//    }
-//    return result;
-//}
-//
-//bool BinaryConverter::decode(const string& bitString,
-//							 vector<unsigned short>& numbers)
-//{
-//    int len = static_cast<int>(bitString.length());
-//    if (len % 16 != 0) {
-//        return false;
-//    }
-//    
-//    // erases all elements of vector
-//    numbers.clear();
-//    
-//    for (int i = 0; i < len; i+=16) {
-//        string result = "";
-//        unsigned short num;
-//        for (int j = i; j < i+16; j++) {
-//            if (bitString[j] == '\t') {
-//                result += '1';
-//            }
-//            else if (bitString[j] == ' ') {
-//                result += '0';
-//            }
-//            // if not tab or space character
-//            else {
-//                return false;
-//            }
-//        }
-//        convertBitStringToNumber(result, num);
-//        numbers.push_back(num);
-//    }
-//    
-//	return false;  // This compiles, but may not be correct
-//}
-//
-//string convertNumberToBitString(unsigned short number)
-//{
-//	string result(BITS_PER_UNSIGNED_SHORT, '0');
-//	for (size_t k = BITS_PER_UNSIGNED_SHORT; number != 0; number /= 2)
-//	{
-//		k--;
-//		if (number % 2 == 1)
-//			result[k] = '1';
-//	}
-//	return result;
-//}
-//
-//bool convertBitStringToNumber(string bitString, unsigned short& number)
-//{
-//	if (bitString.size() != BITS_PER_UNSIGNED_SHORT)
-//		return false;
-//	number = 0;
-//	for (size_t k = 0; k < bitString.size(); k++)
-//	{
-//		number *= 2;
-//		if (bitString[k] == '1')
-//			number++;
-//		else if (bitString[k] != '0')
-//			return false;
-//	}
-//	return true;
-//}
+#include "provided.h"
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+const size_t BITS_PER_UNSIGNED_SHORT = 16;
+
+string convertNumberToBitString(unsigned short number);
+bool convertBitStringToNumber(string bitString, unsigned short& number);
+
+string BinaryConverter::encode(const vector<unsigned short>& numbers)
+{
+    string result = "";
+    for (auto& num : numbers) {
+        result += convertNumberToBitString(num);
+    }
+    
+    for (int i = 0; i < result.length(); i++) {
+        if (result[i] == '1') {
+            result[i] = '\t';
+        }
+        else if (result[i] == '0') {
+            result[i] = ' ';
+        }
+        // shouldn't be any other character
+        else {
+            return "Error! Not a BitString.";
+        }
+    }
+    return result;
+}
+
+bool BinaryConverter::decode(const string& bitString,
+							 vector<unsigned short>& numbers)
+{
+    int len = static_cast<int>(bitString.length());
+    if (len % 16 != 0) {
+        return false;
+    }
+    
+    // erases all elements of vector
+    numbers.clear();
+    
+    for (int i = 0; i < len; i+=16) {
+        string result = "";
+        unsigned short num;
+        for (int j = i; j < i+16; j++) {
+            if (bitString[j] == '\t') {
+                result += '1';
+            }
+            else if (bitString[j] == ' ') {
+                result += '0';
+            }
+            // if not tab or space character
+            else {
+                return false;
+            }
+        }
+        convertBitStringToNumber(result, num);
+        numbers.push_back(num);
+    }
+    
+	return false;  // This compiles, but may not be correct
+}
+
+string convertNumberToBitString(unsigned short number)
+{
+	string result(BITS_PER_UNSIGNED_SHORT, '0');
+	for (size_t k = BITS_PER_UNSIGNED_SHORT; number != 0; number /= 2)
+	{
+		k--;
+		if (number % 2 == 1)
+			result[k] = '1';
+	}
+	return result;
+}
+
+bool convertBitStringToNumber(string bitString, unsigned short& number)
+{
+	if (bitString.size() != BITS_PER_UNSIGNED_SHORT)
+		return false;
+	number = 0;
+	for (size_t k = 0; k < bitString.size(); k++)
+	{
+		number *= 2;
+		if (bitString[k] == '1')
+			number++;
+		else if (bitString[k] != '0')
+			return false;
+	}
+	return true;
+}
--- Projects/4. steg-o-matic/4. steg-o-matic/main.cpp
+++ Projects/4. steg-o-matic/4. steg-o-matic/main.cpp
@@ -2,29 +2,39 @@
 
 //#include "provided.h"
 #include <iostream>
-#include "HashTable.h"
+#include "provided.h"
+#include <string>
+
 using namespace std;
 
 int main()
 {
-    HashTable<string, unsigned int> hash(10, 20);
-    hash.set("hello", 69);
+//    HashTable<string, unsigned int> hash(10, 20);
+//    hash.set("hello", 69);
+//    
+//    unsigned int result = 15;
+//    hash.get("hello", result);
+//    
+////    cout << result << endl;
+//    
+//    hash.set("bye", 111);
+//    hash.set("hi", 52);
+//    
+//    hash.touch("bye"); 
+//    
+//    string k = "";
+//    unsigned int i = 4;
+//    
+//    hash.discard(k, i);
+//    cout << k << i;
     
-    unsigned int result = 15;
-    hash.get("hello", result);
+    vector<unsigned short> v;
+    v.push_back(15);
+    v.push_back(17);
+    v.push_back(66);
     
-//    cout << result << endl;
-    
-    hash.set("bye", 111);
-    hash.set("hi", 52);
-    
-    hash.touch("bye"); 
-    
-    string k = "";
-    unsigned int i = 4;
-    
-    hash.discard(k, i);
-    cout << k << i;
+    string s = BinaryConverter::encode(v);
+    cout << s;
     
     
 	cout << "Test driver not yet written." << endl;
