d3fa60a83dc0b209d145ec13c3a453e93bf2b5c9
Chang Liu <shampliu@gmail.com>
2016-04-12 01:27:52 -0700

    lab1 finished

--- CS 111 Operating Systems/lab1/lab1a.c
+++ CS 111 Operating Systems/lab1/lab1a.c
@@ -11,14 +11,30 @@
 #include <sys/wait.h>
 
 struct termios saved_attributes;
+pid_t CHILD_PID; 
 
-void reset_input_mode(void)
-{
+void reset_input_mode(void) {
+  // printf("RESETTING INPUT MODES\n");
   tcsetattr (0, TCSANOW, &saved_attributes);
 }
 
-void set_input_mode(void)
-{
+void shell_exit(void) {
+  // printf("SHELL EXIT FUNCTION IS CALLED\n");
+  int chld_state; 
+  pid_t rc_pid = waitpid( CHILD_PID, &chld_state, 0);
+  if (rc_pid > 0)
+  {
+    if (WIFEXITED(chld_state)) {
+      printf("Child exited with RC=%d\n", WEXITSTATUS(chld_state));
+    }
+    else {
+      printf("Child process aborted abnormally\n"); 
+    }
+  }
+
+}
+
+void set_input_mode(void) {
   struct termios tattr;
   char *name;
 
@@ -42,18 +58,22 @@ void set_input_mode(void)
 }
 
 void SIGPIPE_handler(int signum) {
+  exit(1);
 }
 
+void SIGINT_handler(int signum) {}
+
+void SIGHUP_handler(int signum) {
+  exit(1);
+}
+
+// thread reads input from shell pipe and writes to stdout
 void * thread_func(void *fd) {
   char buf[128];
   int size; 
   while ((size = read(*(int *) fd, buf, 128)) > 0) {
-    if (buf[0] == '\004') {
-      // restore terminal mode and exit 1
-    }
     write(1, buf, size);
   }
-
 
 }
 
@@ -67,11 +87,14 @@ int main(int argc, char **argv)
   int o;
   int options_index = 0;
 
+  set_input_mode();
+
   while((o = getopt_long(argc, argv,"", long_options, &options_index)) != -1) {
   }
 
   if (shell) {
-    int pipe_from_shell[2]; 		// shell is the child process
+
+    int pipe_from_shell[2]; 		
     int pipe_to_shell[2];
     pid_t pid = -1;
 
@@ -83,98 +106,95 @@ int main(int argc, char **argv)
       fprintf(stderr, "ERROR: pipe() failed!\n");
       exit(1);
     }
+
+    signal(SIGINT, SIGINT_handler);
+    signal(SIGHUP, SIGHUP_handler);
+    signal(SIGPIPE, SIGPIPE_handler);
    
     pid = fork();
-    if (pid == 0) { 			// shell process
+    // shell process, child process
+    if (pid == 0) { 			
+      
+
       close(pipe_from_shell[0]);
       dup2(pipe_from_shell[1], 1); 
       close(pipe_to_shell[1]);
       dup2(pipe_to_shell[0], 0);
-      execl("/bin/bash", "/bin/bash", NULL); 
+      // execl("/bin/bash", "/bin/bash", NULL); 
+      char *execvp_argv[2];
+      char execvp_filename[] = "/bin/bash";
+      execvp_argv[0] = execvp_filename;
+      execvp_argv[1] = NULL;
+      execvp(execvp_filename, execvp_argv); 
     }
-    else if (pid > 0) {			// terminal process
-      // setup SIGPIPE handler using signal
-      // signal(SIGPIPE, SIGPIPE_handler);
+    // terminal process, parent process
+    // pid is pid of child process
+    else if (pid > 0) {	
+      CHILD_PID = pid; 	
+      atexit(shell_exit);	
+
       // terminal doesn't use read end of pipe_to_shell
       close(pipe_to_shell[0]);
       // terminal doesn't use write end of pipe_from_shell
       close(pipe_from_shell[1]); 
       
       pthread_t t1;
-      // int fd = (int) pipe_to_shell[1];
       int fd = (int) pipe_from_shell[0];
       int *arg = malloc(sizeof(*arg));
       *arg = fd; 
 
       if (pthread_create(&t1, NULL, thread_func, arg) != 0) {
-         printf("ERROR: Unable to create new thread");
+         printf("ERROR: Unable to create new thread\n");
          exit(-1);
       }
 
       char buf[128];
       int size = 0;
-      char crlf[2];
-      crlf[0] = '\015';
-      crlf[1] = '\012'; 
 
+      // thread reads from keyboard, echoes to stdout and forwards to shell
       while ((size = read(0, buf, 128)) > 0) {
-        if (buf[0] == '\004')
-          break;
+        // CTRL-C
+        if (buf[0] == '\003') {
+          kill(pid, SIGINT);
+        }
+        // CTRL-D
+        else if (buf[0] == '\004') {
+          close(pipe_to_shell[1]);
+          close(pipe_from_shell[0]); 
+          // sleep(2);
+          kill(pid, SIGHUP);
+          exit(0);
+        }
+        // <cr> or <lf>
         else if (buf[0] == '\015' || buf[0] == '\012') {
-          write(1, crlf, 2);
-          write(pipe_to_shell[1], crlf, 2);
+          write(1, "\015\012", 2);
+          write(pipe_to_shell[1], "\012", 1); // goes to shell only as <lf>
         }
         else {
           write(1, buf, size);
           write(pipe_to_shell[1], buf, size);
         }
       } 
-      // while ((size = read(0, buf, 128)) > 0) {
-      //   write(pipe_to_shell[1], buf, size);
-      //   size = read(pipe_from_shell[0], buf, 128);
-      //   write(1, buf, size);
-      // }
-
-
-      // pthread_join(thread1, NULL);
-      // pthread_exit(NULL);
-      
-      /*
-      char buf[128];    
-      int size = 0;
-      while ((size = read(0, buf, 128)) > 0) {
-        write(pipe_to_shell[1], buf, size);
-        size = read(pipe_from_shell[0], buf, 128);
-        write(1, buf, size);
-      }
-      */
     }
     else {
       fprintf(stderr, "ERROR: fork() failed!\n");
       exit(1);
     }
-    
 
     exit(0);
   }
 
-  char c;
-
-  set_input_mode();
-
-  char crlf[2];
-  crlf[0] = '\015';
-  crlf[1] = '\012'; 
   char buf[128];
   int size = 0;
+
+  // \015 is <cr>, \012 is <lf>
   while ((size = read(0, buf, 128)) > 0) {
     if (buf[0] == '\004')
       break;
     else if (buf[0] == '\015' || buf[0] == '\012') {
-      write(1, crlf, 2);
+      write(1, "\015\012", 2);
     }
     else 
       write(1, buf, size);
   } 
-
 }
