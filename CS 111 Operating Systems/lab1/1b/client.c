2f7d5312b2cf7064a4e20cb1e5068f9d3a4361fa
Chang Liu <shampliu@gmail.com>
2016-04-14 01:35:56 -0700

    noncanonical input

--- CS 111 Operating Systems/lab1/1b/client.c
+++ CS 111 Operating Systems/lab1/1b/client.c
@@ -4,67 +4,124 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <getopt.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
 void error(char *msg)
 {
-    perror(msg);
-    exit(0);
+  perror(msg);
+  exit(0);
 }
+
+struct termios saved_attributes;
+
+void reset_input_mode(void) {
+  tcsetattr (0, TCSANOW, &saved_attributes);
+}
+
+void set_input_mode(void) {
+  struct termios tattr;
+  char *name;
+
+  /* Make sure stdin is a terminal. */
+  if (!isatty (0))
+  {
+    fprintf (stderr, "Not a terminal.\n");
+    exit (EXIT_FAILURE);
+  }
+
+  /* Save the terminal attributes so we can restore them later. */
+  tcgetattr (0, &saved_attributes);
+  atexit (reset_input_mode);
+
+  /* Set the terminal modes. */
+  tcgetattr (0, &tattr);
+  tattr.c_lflag &= ~(ICANON|ECHO);  // clear ICANON and ECHO 
+  tattr.c_cc[VMIN] = 1;     // min bytes that must be available for input 
+  tattr.c_cc[VTIME] = 0;    // how long to wait for input
+  tcsetattr (0, TCSAFLUSH, &tattr);
+}
+
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
+  set_input_mode();
 
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+  int sockfd, portno, n;
 
-    char buffer[256];
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
 
-    static struct option long_options[] = {
-      { "port", required_argument, NULL, 'p' }
-    };
+  char buf[256];
 
-    int options_index = 0;
-    int c;
+  static struct option long_options[] = {
+    { "port", required_argument, NULL, 'p' },
+    { "log" , required_argument, NULL, 'l' }
+  };
 
-    while((c = getopt_long(argc, argv, "", long_options, &options_index)) != -1) {
-      switch(c) {
-        case 'p':
-          if (optarg) { 
-            portno = atoi(optarg);
-          }
-          break;
+  int options_index = 0;
+  int c;
+
+  while((c = getopt_long(argc, argv, "", long_options, &options_index)) != -1) {
+    switch(c) {
+      case 'p':
+      if (optarg) { 
+        portno = atoi(optarg);
+      }
+      break;
+      case 'l':
+      if (optarg) {
+            // HANDLE LOG OPTION
       }
     }
+  }
 
-    char hostname[] = "localhost";
-    server = gethostbyname(hostname);
+  char hostname[] = "localhost";
+  server = gethostbyname(hostname);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  if (server == NULL) {
+    fprintf(stderr,"ERROR, no such host\n");
+    exit(0);
+  }
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr, 
+   (char *)&serv_addr.sin_addr.s_addr,
+   server->h_length);
+  serv_addr.sin_port = htons(portno);
+  if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+    error("ERROR connecting");
+
+  bzero(buf,256);
+
+  int size = 0;
+
+  // \015 is <cr>, \012 is <lf>
+  while ((size = read(0, buf, 128)) > 0) {
+    if (buf[0] == '\004')
+      break;
+    else if (buf[0] == '\015' || buf[0] == '\012') {
+      write(1, "\015\012", 2);
+      write(sockfd, "\015\012", 2);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    return 0;
+    else {
+      write(1, buf, size);
+      write(sockfd, buf, size);
+    }
+  }
+  // fgets(buf,255,stdin);
+  // n = write(sockfd,buf,strlen(buf));
+  // if (n < 0) 
+  //   error("ERROR writing to socket");
+  // bzero(buf,256);
+  // n = read(sockfd,buf,255);
+  // if (n < 0) 
+  //   error("ERROR reading from socket");
+  // printf("%s\n",buf);
+  return 0;
 }
--- CS 111 Operating Systems/lab1/1b/server.c
+++ CS 111 Operating Systems/lab1/1b/server.c
@@ -1,10 +1,13 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
 #include <stdio.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h> 
 #include <getopt.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
 void error(char *msg)
 {
@@ -15,7 +18,7 @@ void error(char *msg)
 int main(int argc, char *argv[])
 {
   int sockfd, newsockfd, portno, clilen;
-  char buffer[256];
+  char buf[256];
   struct sockaddr_in serv_addr, cli_addr;
   int n;
 
@@ -39,6 +42,7 @@ int main(int argc, char *argv[])
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) 
     error("ERROR opening socket");
+
   bzero((char *) &serv_addr, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -51,11 +55,29 @@ int main(int argc, char *argv[])
   newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
   if (newsockfd < 0) 
     error("ERROR on accept");
-  bzero(buffer,256);
-  n = read(newsockfd,buffer,255);
-  if (n < 0) error("ERROR reading from socket");
-  printf("Here is the message: %s\n",buffer);
-  n = write(newsockfd,"I got your message",18);
-  if (n < 0) error("ERROR writing to socket");
+  bzero(buf,256);
+
+  int size = 0;
+
+  // \015 is <cr>, \012 is <lf>
+  while ((size = read(newsockfd, buf, 128)) > 0) {
+    if (buf[0] == '\004')
+      break;
+    // else if (buf[0] == '\015' || buf[0] == '\012') {
+    //   write(1, "\015\012", 2);
+    //   write(sockfd, "\015\012", 2);
+    // }
+    // else {
+    //   write(1, buf, size);
+    //   write(sockfd, buf, size);
+    // }
+    write(1, buf, size); 
+  }
+
+  // n = read(newsockfd,buf,255);
+  // if (n < 0) error("ERROR reading from socket");
+  // printf("Here is the message: %s\n",buf);
+  // n = write(newsockfd,"I got your message",18);
+  // if (n < 0) error("ERROR writing to socket");
   return 0; 
 }
